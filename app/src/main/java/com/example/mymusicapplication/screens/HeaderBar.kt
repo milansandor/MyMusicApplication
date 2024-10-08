1328b166e638e07ce7fb454bfb32f827c626735b
milansandor <sandor.szoda.milan@gmail.com>
2024-10-08 22:28:24 +0200

    tag-list: smaller padding in between tag lists

--- app/src/main/java/com/example/mymusicapplication/screens/HeaderBar.kt
+++ app/src/main/java/com/example/mymusicapplication/screens/HeaderBar.kt
@@ -1,6 +1,5 @@
 package com.example.mymusicapplication.screens
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,7 +15,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
@@ -24,15 +22,13 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mymusicapplication.models.Album
 
 
@@ -131,7 +127,7 @@ fun TagList(
         Column (
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp, 0.dp, 16.dp, 16.dp)
+                .padding(16.dp, 0.dp, 16.dp, 5.dp)
         ) {
             LazyColumn(
                 modifier = Modifier
@@ -148,8 +144,7 @@ fun TagList(
                     )
                 }
             }
-            Spacer(modifier = Modifier.height(4.dp))
-
+            Spacer(modifier = Modifier.height(2.dp))
             SearchAlbumButton()
         }
     }
@@ -165,14 +160,18 @@ fun TagItem(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .fillMaxWidth()
-            .padding(bottom = 1.dp)
+            .padding(bottom = 10.dp)
     ) {
         Checkbox(
             checked = isChecked,
-            onCheckedChange = onCheckedChange
+            onCheckedChange = onCheckedChange,
+            modifier = Modifier.size(20.dp)
         )
         Spacer(modifier = Modifier.width(8.dp))
-        Text(text = tag)
+        Text(
+            text = tag,
+            fontSize = 12.sp,
+        )
     }
 }
 
